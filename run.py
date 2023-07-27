313d9d36894175ef46addbe39b2eea253d70a6b6
Stephen Dawson <stephendawson@protonmail.com>
2023-07-27 13:01:49 +0100

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+name = input('What is your name? ')
+print(f"Hello, {name}!")
