d5ce1d452300160a8d7424270807df96aef84525
Stephen Dawson <stephendawson@protonmail.com>
2023-07-27 17:49:39 +0100

    import dependencies, add scope and main function

--- run.py
+++ run.py
@@ -1,5 +1,26 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-name = input('What is your name? ')
-print(f"Hello, {name}!")
+import gspread
+from google.oauth2.service_account import Credentials
+
+# Define the scope
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+]
+
+# Define the credentials
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('ConsoleConnections')
+
+
+def main():
+    """
+    Run all program functions
+    """
+    name = input('What is your name? ')
+    print(f"Hello, {name}!")
+
+
+main()
