cfd74c585d48749fcf94ded1c0b5ad7aaf39102c
Stephen Dawson <stephendawson@protonmail.com>
2023-07-27 17:56:55 +0100

    add user type function and call in main()

--- run.py
+++ run.py
@@ -15,12 +15,26 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('ConsoleConnections')
 
 
+def establish_user_type():
+    """
+    Establish if the user is a real user or a test user
+    """
+    user_type = input(
+        "Are you just testing the app or here to find a connection?\n 1. Tester\n 2. Real User\n")
+
+    if user_type == "1":
+        print("Welcome to the test version - feel free to play around and make some connections with imaginary people.")
+    elif user_type == "2":
+        print("Welcome to the user version of the app")
+    else:
+        print("Please enter either 'test' or 'user' to continue")
+
+
 def main():
     """
     Run all program functions
     """
-    name = input('What is your name? ')
-    print(f"Hello, {name}!")
+    establish_user_type()
 
 
 main()
