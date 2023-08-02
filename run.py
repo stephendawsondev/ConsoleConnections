2f3eeb6f88813ce7870b4c598fa9e1976d03db05
Stephen Dawson <stephendawson@protonmail.com>
2023-08-02 08:26:55 +0100

    autopep8 formatting

--- run.py
+++ run.py
@@ -19,20 +19,30 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('ConsoleConnections')
 
-CONSOLE_CONNECTIONS_HEADING = """     ,gggg,                                                                     ,gggg,
-   ,88YYYY8b,                                               ,dPYb,            ,88YYYY8b,                                                          I8
-  d8"     `Y8                                               IP'`Yb           d8"     `Y8                                                          I8
- d8'   8b  d8                                               I8  8I          d8'   8b  d8                                                       88888888 gg
-,8I    "Y88P'                                               I8  8'         ,8I    "Y88P'                                                          I8    ""
-I8'            ,ggggg,    ,ggg,,ggg,     ,g,      ,ggggg,   I8 dP  ,ggg,   I8'            ,ggggg,    ,ggg,,ggg,   ,ggg,,ggg,   ,ggg,     ,gggg,   I8    gg     ,ggggg,    ,ggg,,ggg,     ,g,
-d8            dP"  "Y8ggg,8" "8P" "8,   ,8'8,    dP"  "Y8gggI8dP  i8" "8i  d8            dP"  "Y8ggg,8" "8P" "8, ,8" "8P" "8, i8" "8i   dP"  "Yb  I8    88    dP"  "Y8ggg,8" "8P" "8,   ,8'8,
-Y8,          i8'    ,8I  I8   8I   8I  ,8'  Yb  i8'    ,8I  I8P   I8, ,8I  Y8,          i8'    ,8I  I8   8I   8I I8   8I   8I I8, ,8I  i8'       ,I8,   88   i8'    ,8I  I8   8I   8I  ,8'  Yb
-`Yba,,_____,,d8,   ,d8' ,dP   8I   Yb,,8'_   8),d8,   ,d8' ,d8b,_ `YbadP'  `Yba,,_____,,d8,   ,d8' ,dP   8I   Yb,dP   8I   Yb,`YbadP' ,d8,_    _,d88b,_,88,_,d8,   ,d8' ,dP   8I   Yb,,8'_   8)
-  `"Y8888888P"Y8888P"   8P'   8I   `Y8P' "YY8P8P"Y8888P"   8P'"Y8888P"Y888   `"Y8888888P"Y8888P"   8P'   8I   `Y8P'   8I   `Y888P"Y888P""Y8888PP8P""Y88P""Y8P"Y8888P"   8P'   8I   `Y8P' "YY8P8P
-                                                                                                                                                                                                """
+CONSOLE_CONNECTIONS_HEADING = """
+                ,gggg,
+               ,88^^^Y8b,                                           ,dPYb,
+              d8"     `Y8                                           IP'`Yb
+             d8'   8b  d8                                           I8  8I
+            ,8I    "Y88P'                                           I8  8'
+            I8'           ,ggggg,   ,ggg,,ggg,    ,g,      ,ggggg,  I8 dP  ,ggg,
+            d8           dP"  "Y8gg,8" "8P" "8,  ,8'8,    dP"  "Y8ggI8dP  i8" "8i
+            Y8,         i8'    ,8I I8   8I   8I ,8'  Yb  i8'    ,8I I8P   I8, ,8I
+            `Yba,,_____,d8,   ,d8',dP   8I   Yb,8'_   8),d8,   ,d8',d8b,_ `YbadP'
+              `"Y,gggg,P"Y8888P"  8P'   8I   `YP' "YY8P8P"Y8888P"  8P'"Y8888P"Y888
+     ,gggg,
+   ,88^^^Y8b,                                                      I8
+  d8"     `Y8                                                      I8
+ d8'   8b  d8                                                   88888888gg
+,8I    "Y88P'                                                      I8   ""
+I8'           ,ggggg,   ,ggg,,ggg,   ,ggg,,ggg,   ,ggg,    ,gggg,  I8   gg    ,ggggg,   ,ggg,,ggg,    ,g,
+d8           dP"  "Y8gg,8" "8P" "8, ,8" "8P" "8, i8" "8i  dP"  "Yb I8   88   dP"  "Y8gg,8" "8P" "8,  ,8'8,
+Y8,         i8'    ,8I I8   8I   8I I8   8I   8I I8, ,8I i8'      ,I8,  88  i8'    ,8I I8   8I   8I ,8'  Yb
+`Yba,,_____,d8,   ,d8',dP   8I   Yb,dP   8I   Yb,`YbadP',d8,_    ,d88b_,88,,d8,   ,d8',dP   8I   Yb,8'_   8)
+  `"Y888888P"Y8888P"  8P'   8I   `Y8P'   8I   `Y888P"Y88P""Y8888P8P""Y8P""YP"Y8888P"  8P'   8I   `YP' "YY8P8P """
 
 
-APP_SUBHEADING = """\t\t\t\t\t\t\t\t\t\t\u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764\n\n\t\t\t\t\t\t\t\t\t\tThere's no cover to judge here!\n\n\t\t\t\t\t\t\t\t\t\t\u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764\n"""
+APP_SUBHEADING = "\t\t\t\t\u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764\n\n\t\t\t\tThere's no cover to judge here!\n\n\t\t\t\t\u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764\n"
 
 
 class User:
@@ -929,9 +939,9 @@ def main():
     """
     Run all program functions
     """
-    # clear_terminal()
-    # print(
-    #     f"\t\t\t\t\t\t\t\t\t\t\tWelcome to\n\n{CONSOLE_CONNECTIONS_HEADING}\n{APP_SUBHEADING}")
+    clear_terminal()
+    print(
+        f"t\t\t\t\t\tWelcome to\n\n{CONSOLE_CONNECTIONS_HEADING}\n{APP_SUBHEADING}")
     # [user_data, worksheet_selected] = establish_user_data()
     # user = present_login_signup_step(user_data, worksheet_selected)
     # present_main_menu(user, worksheet_selected)
@@ -960,7 +970,7 @@ def main():
             'Staying home'],
         2)
 
-    user.view_top_matches("test_users")
+    # user.view_top_matches("test_users")
 
 
 main()
