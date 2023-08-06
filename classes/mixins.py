92dc48c4ca05c2caefbf52926580dd1d5fde7b40
Stephen Dawson <stephendawson@protonmail.com>
2023-08-06 17:03:22 +0100

    add time delay to clear terminal mixin

--- classes/mixins.py
+++ classes/mixins.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 
 class ClearTerminalMixin():
@@ -6,10 +7,11 @@ class ClearTerminalMixin():
     Used in multiple files and clears the terminal when called.
     """
     @staticmethod
-    def clear_terminal():
+    def clear_terminal(delay=0):
         """
         Clears the terminal window.
         """
+        time.sleep(delay)
         # https://www.delftstack.com/howto/python/python-clear-console/
         command = 'clear'
         if os.name in (
