af09d7048545dd7acc3c7074febc3f11aa430b87
Stephen Dawson <stephendawson@protonmail.com>
2023-08-06 10:10:30 +0100

    shorten line lengths, remove global data variable

--- classes/profile.py
+++ classes/profile.py
@@ -30,13 +30,15 @@ class Profile():
 
         age = int(age)
         if age < 18:
-            print(
-                "\nSorry! You must be at least 18 years old to use Console Connections.\n")
+            print("""
+Sorry! You must be at least 18 years old to use Console Connections.
+""")
             return Profile.prompt_for_age()
 
         if age >= 100:
-            print(
-                "\nWhile we admire your vitality, users must between the ages of 18 and 100.\n")
+            print("""
+While we admire your vitality, users must between the ages of 18 and 100
+""")
             return Profile.prompt_for_age()
 
         return age
@@ -46,8 +48,11 @@ class Profile():
         """
         Gets the user's gender.
         """
-        gender_input = input(
-            "\nPlease input the gender you identify as: \n\n1. Male\t2. Female\t3. Non-binary\n").lower()
+        gender_input = input("""
+Please input the gender you identify as:
+
+1. Male     2. Female     3. Non-binary
+""").lower()
 
         if gender_input == "1" or gender_input == "male":
             gender = "Male"
@@ -56,7 +61,9 @@ class Profile():
         elif gender_input == "3" or gender_input == "non-binary":
             gender = "Non-binary"
         else:
-            print("\nPlease enter one of the above genders using 1, 2, or 3.\n")
+            print("""
+Please enter one of the above genders using 1, 2, or 3.
+""")
             return Profile.prompt_for_gender()
 
         return gender
@@ -66,15 +73,17 @@ class Profile():
         Sets the minimum and maximum age that the user wants to match with.
         Minimum must be 18 and maximum is 100.
         """
-        min_age = input(
-            "\nWhat is the minimum age you would like to match with? The age must be 18 or over.\n")
+        min_age = input("""
+What is the minimum age you want to match with? The age must be 18 or over.
+""")
 
         if not min_age.isdigit():
             print("\nPlease enter a number between 18 and 100\n")
             return self.set_age_range_seeking()
 
-        max_age = input(
-            "\nWhat is the maximum age you would like to match with? The age must be 18 or over.\n")
+        max_age = input("""
+What is the maximum age you want to match with? The age must be 18 or over.
+""")
 
         if not max_age.isdigit():
             print("\nPlease enter a number between 18 and 100\n")
@@ -102,22 +111,27 @@ class Profile():
             for gender in current_genders:
                 print(gender)
 
-            action = input(
-                "\nDo you want to (a)dd or (r)emove genders or (q)uit?\n").lower()
+            action = input("""
+Do you want to (a)dd or (r)emove genders or (q)uit?
+""").lower()
 
             if action == 'a' and len(current_genders) < len(available_genders):
                 print("\nAvailable genders to add:\n")
                 for index, gender in enumerate(
-                        [gender for gender in available_genders if gender not in current_genders], start=1):
+                    [gender for gender in available_genders
+                     if gender not in current_genders],
+                        start=1):
                     print(f"{index}. {gender}\n")
                 add_gender = input(
                     "Enter the number of the gender you want to add:\n")
                 try:
                     chosen_gender = [
-                        gender for gender in available_genders if gender not in current_genders][int(add_gender) - 1]
+                        gender for gender in available_genders
+                        if gender not in current_genders][int(add_gender) - 1]
                     current_genders.append(chosen_gender)
-                    print(
-                        f"{chosen_gender} has been added to your gender seeking preferences.")
+                    print(f"""
+ {chosen_gender} has been added to your gender seeking preferences.
+""")
                 except (IndexError, ValueError):
                     print("Invalid choice. Please try again.")
 
@@ -130,14 +144,17 @@ class Profile():
                 try:
                     chosen_gender = current_genders[int(remove_gender) - 1]
                     current_genders.remove(chosen_gender)
-                    print(
-                        f"{chosen_gender} has been removed from your gender seeking preferences.")
+                    print(f"""
+{chosen_gender} has been removed from your gender seeking preferences.
+""")
                 except (IndexError, ValueError):
                     print("Invalid choice. Please try again.")
 
             elif action == 'q':
                 if len(current_genders) == 0:
-                    print("You need to add at least one gender to your preferences.")
+                    print("""
+You need to add at least one gender to your preferences.
+""")
                 else:
                     return current_genders
             else:
@@ -167,14 +184,17 @@ Editable:
     6. Save and exit
         """)
 
-            edit_profile_option = input(
-                "Please enter the number of the field you would like to edit,\nor enter '6' to save and exit:\n")
+            edit_profile_option = input("""
+Please enter the number of the field you would like to edit,
+or enter '6' to save and exit:
+""")
             if edit_profile_option == "1":
                 authentication = Authentication()
                 self.user.password = authentication.prompt_for_password(
                     "updating")
             elif edit_profile_option == "2":
-                self.user.security_questions_and_answers = Authentication.prompt_for_security_questions_and_answers()
+                auth_check = Authentication.prompt_for_security_questions()
+                self.user.security_questions_and_answers = auth_check
             elif edit_profile_option == "3":
                 self.user.bio = input("\nPlease enter your bio:\n")
             elif edit_profile_option == "4":
--- classes/user_access.py
+++ classes/user_access.py
@@ -1,12 +1,13 @@
+"""Module responsible for handling random number generation
+in the signup process."""
 import random
+
 from classes.worksheet import Worksheet
 from classes.profile import Profile
 from classes.authenticaton import Authentication
 from classes.user import User
 from classes.mixins import ClearTerminalMixin
 from classes.main_menu import MainMenu
-
-DATA = None
 
 
 class UserAccess():
@@ -26,56 +27,60 @@ class UserAccess():
         - If a real user, pull data from the real user worksheet.
         """
 
-        global DATA
-
         while True:
 
             user_type = input(
-                "\nAre you just testing the app or here to find a connection? Please enter 1 or 2.\n\n1. Tester\t2. Real User\n")
+                """
+Are you testing or here to find a connection? Please enter 1 or 2.
+
+1. Tester     2. Real User
+""")
 
             if user_type == "1":
-                print("\nWelcome to the test version of the app - feel free to play\naround and make some connections with imaginary people.\n")
+                print("""
+Welcome to the test version of the app - feel free to play
+around and make some connections with imaginary people.
+""")
 
                 Worksheet.set_worksheet("test_users")
-                selected_worksheet = Worksheet()
-                DATA = selected_worksheet.get_all_values()
-
                 break
 
             elif user_type == "2":
-                print(
-                    "\nWelcome to the real version of the app - let's make some connections!\n")
+                print("""
+Welcome to the real version of the app - let's make some connections!
+""")
                 Worksheet.set_worksheet("real_users")
-                selected_worksheet = Worksheet()
-                DATA = selected_worksheet.get_all_values()
                 break
 
             else:
-                print("Please enter either '1' or '2' to continue\n")
+                print("\nPlease enter either '1' or '2' to continue.\n")
 
         return self.present_login_signup_step()
 
-    def user_signup(self):
+    def user_signup(self, user_data):
         """
-        Runs generate usercode, password, alias and security question functions.
-        - Adds the user to the Google sheet.
-        """
+Runs generate usercode, password, alias and security question functions.
+- Adds the user to the Google sheet."""
 
-        usercode = self.generate_random_usercode()
+        usercode = self.generate_random_usercode(user_data)
 
         print(
-            f"If you complete the signup process, your usercode will be {usercode}. Please keep this safe as you will need it to login.\n")
+            f"""
+If you complete the signup process, your usercode will be {usercode}.
+Please keep this safe as you will need it to login.
+""")
 
         authentication = Authentication()
         password = authentication.prompt_for_password("new")
 
-        alias = self.create_and_validate_alias()
+        alias = self.create_and_validate_alias(user_data)
 
         ClearTerminalMixin.clear_terminal()
 
         print("Great! We've got your username, password and alias!\n")
 
-        security_questions_and_answers = Authentication.prompt_for_security_questions_and_answers()
+        auth_check = Authentication.prompt_for_security_questions()
+        security_questions_and_answers = auth_check
 
         ClearTerminalMixin.clear_terminal()
 
@@ -85,7 +90,7 @@ class UserAccess():
 
         gender = Profile.prompt_for_gender()
 
-        row_num = len(DATA) + 1
+        row_num = len(user_data) + 1
 
         # create user object
         user = User(
@@ -113,7 +118,7 @@ class UserAccess():
 
         return main_menu.present_main_menu(user)
 
-    def generate_random_usercode(self):
+    def generate_random_usercode(self, user_data):
         """
         - Generates a new 6-digit usercode for the user.\n
         - Checks if the usercode already exists in the database.\n
@@ -122,46 +127,54 @@ class UserAccess():
         usercode = random.randint(100000, 999999)
 
         # check if the usercode already exists in the database
-        for row in DATA:
+        for row in user_data:
             if row[0] == str(usercode):
                 # if the usercode exists, generate a new one
-                self.generate_random_usercode()
+                self.generate_random_usercode(user_data)
         return usercode
 
-    def create_and_validate_alias(self):
+    def create_and_validate_alias(self, user_data):
         """
         Checks the user's alias against the Google Sheet.
         - Make sure the alias is no more than 16 characters.
         - If the alias exists, prompt the user to come up with a new alias.
         - If the alias doesn't exist, return the alias.
         """
-        alias = input("\nYou will also need to create an alias, which may be used for in-app communication.\nThe alias must be unique and it is recommended that it does not easily identify you (for security reasons).\nPlease enter your alias now:\n")
+        alias = input("""
+You will also need to create an alias to use for in-app communication.
+The alias must be unique and it is recommended that it does not easily
+identify you (for security reasons).
+                      
+Please enter your alias now:
+""")
 
         # check if the alias is greater than 3 but fewer than 16 characters
         while len(alias) < 3 or len(alias) > 16:
-            alias = input(
-                "\nAlias must be greater than 3 but fewer than 16 characters. Please try again:\n")
+            alias = input("""
+Alias must be greater than 3 but fewer than 16 characters. Please try again:
+""")
 
         # check if the alias already exists in the database
-        for row in DATA:
+        for row in user_data:
             if row[2] == alias:
                 # if the alias exists, prompt the user to come up with a new
                 # alias
                 print("\nAlias already exists.\n")
-                return self.create_and_validate_alias()
+                return self.create_and_validate_alias(user_data)
         return alias
 
-    def user_login(self):
+    def user_login(self, all_users):
         """
-        Checks if the user exists on the Google Sheet.\n
-        - If the user exists, prompt them for password.\n
-        - If the user doesn't exist, let them know and ask if they want to signup.\n
+Checks if the user exists on the Google Sheet.\n
+- If the user exists, prompt them for password.\n
+- If the user doesn't exist, let them know and ask if they want to signup.\n
         """
+        all_users = Worksheet().get_all_values()
         user_exists = False
         while user_exists is False:
             usercode_input = input("\nPlease enter your usercode:\n")
             # checks the first column of the data for the usercode
-            for index, row in enumerate(DATA):
+            for index, row in enumerate(all_users):
                 if row[0] == str(usercode_input):
                     user_exists = True
                     # prompt for password
@@ -193,23 +206,29 @@ class UserAccess():
 
     def present_login_signup_step(self):
         """
-        Provide the user with the option to login or signup.\n
-        - If login, check if the user exists in the database and run the login function (if they exist)\n
-        - If signup, check if the user exists in the database and run the signup function (if they don't exist).
+Provide the user with the option to login or signup.\n
+- If login, check if the user exists in the database and
+run the login function (if they exist)
+- If signup, check if the user exists in the database
+and run the signup function (if they don't exist).
         """
         while True:
-            login_signup = input(
-                "Would you like to login or signup? Please enter 1 or 2.\n\n1. Login\t2. Signup\n")
+            login_signup = input("""
+Would you like to login or signup? Please enter 1 or 2.
 
+1. Login     2. Signup
+""")
+
+            all_users = Worksheet().get_all_values()
             if login_signup == "1":
                 ClearTerminalMixin.clear_terminal()
                 print("\nLogin\n")
                 # return data from login function (also breaks the loop)
-                return self.user_login()
-            elif login_signup == "2":
+                return self.user_login(all_users)
+            if login_signup == "2":
                 ClearTerminalMixin.clear_terminal()
                 print("\nSignup\n")
                 # return data from signup function (also breaks the loop)
-                return self.user_signup()
-            else:
-                print("\nPlease enter either '1' to Login or '2' to Signup\n")
+                return self.user_signup(all_users)
+
+            print("\nPlease enter either '1' to Login or '2' to Signup.\n")
